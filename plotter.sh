398499b0be908e7881f3b4ac85076477f24a7349
suisrc <suisrc@outlook.com>
2021-05-14 19:36:35 +0000

    clear

--- plotter.sh
+++ plotter.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # The param
 ARG=`sed -n 1p _ctm.txt`
 if [ ${ARG}x = ''x ]; then
@@ -17,12 +16,12 @@ if [ ${PPK}x = ''x ]; then
     read PPK < _ppk.txt
 fi
 
-echo "chia plots create ${ARG} -t /tmp01 -2 /tmp02 -d /dst -f ${FPK} -p ${PPK}"
+echo "chia plots create ${ARG} -f ${FPK} -p ${PPK}"
 
 while [ ! -f /root/run/stop ]
 do
-  rm /tmp01/*.tmp /tmp02/*.tmp
-  chia plots create ${ARG} -t /tmp01 -2 /tmp02 -d /dst -f ${FPK} -p ${PPK}
-  sleep 30s
+    ./clear.sh
+    chia plots create ${ARG} -f ${FPK} -p ${PPK}
+    sleep 30s
 done
 echo Stopfile found, exiting
